791f4e5568452a94d986dcbf2e55aefadfef2b92
lbq <lbq@LEO-MBP.local>
2018-07-15 14:10:07 +0800

    ll

---
+++ DMMVVM/DMMVVM/DMPlaceInfo.m
@@ -0,0 +1,13 @@
+//
+//  DMPlaceInfo.m
+//  DMMVVM
+//
+//  Created by lbq on 2018/6/8.
+//  Copyright © 2018年 lbq. All rights reserved.
+//
+
+#import "DMPlaceInfo.h"
+
+@implementation DMPlaceInfo
+
+@end
---
+++ DMMVVM/DMMVVM/DMProvinceProtocol.h
@@ -0,0 +1,13 @@
+//
+//  DMProvinceProtocol.h
+//  DMMVVM
+//
+//  Created by lbq on 2018/6/8.
+//  Copyright © 2018年 lbq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DMProvinceProtocol <NSObject>
+
+@end
